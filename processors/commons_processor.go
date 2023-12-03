b7f8623a4bf2db8a72573a94bd4f50f48b47a9db
Gaston Salgado <gastonsalgado@gmail.com>
2023-12-03 20:00:36 -0300

    add configmap processor

---
+++ processors/commons_processor.go
@@ -0,0 +1,30 @@
+package processors
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/record"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type Application interface {
+}
+
+type Reconcile interface {
+	client.Reader
+	client.Writer
+	client.StatusClient
+
+	GetSchema() *runtime.Scheme
+	GetRecorder() record.EventRecorder
+	GetFinalizer() string
+	CheckFinalizer(logr.Logger, context.Context, ctrl.Request, Application) (bool, error)
+}
+
+type Processor interface {
+	IsReady(Reconcile, logr.Logger, context.Context, Application) (bool, error)
+	Process(Reconcile, logr.Logger, context.Context, Application) (bool, error)
+}
---
+++ processors/k8s/config_map_processor.go
@@ -0,0 +1,53 @@
+package k8sprocessor
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/gastonsalgado/k8s-operator-lib/processors"
+	"github.com/go-logr/logr"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type ConfigMapsProcessor struct {
+	Object       *corev1.ConfigMap
+	Action       K8sAction
+	Dependencies []processors.Processor
+}
+
+func (configMapsProcessor *ConfigMapsProcessor) getObject() client.Object {
+	return configMapsProcessor.Object
+}
+
+func (configMapsProcessor *ConfigMapsProcessor) getDependencies() []processors.Processor {
+	return configMapsProcessor.Dependencies
+}
+
+func (configMapsProcessor *ConfigMapsProcessor) getEmptyObject() client.Object {
+	return &corev1.ConfigMap{}
+}
+
+func (configMapsProcessor *ConfigMapsProcessor) compareDiff(compareObject interface{}) string {
+	typeMetaDiff := cmp.Diff(compareObject.(*corev1.ConfigMap).TypeMeta, configMapsProcessor.Object.TypeMeta, cmpopts.IgnoreUnexported())
+	annotationsDiff := cmp.Diff(compareObject.(*corev1.ConfigMap).Annotations, configMapsProcessor.Object.Annotations, cmpopts.IgnoreUnexported())
+	dataDiff := cmp.Diff(compareObject.(*corev1.ConfigMap).Data, configMapsProcessor.Object.Data, cmpopts.IgnoreUnexported())
+	return fmt.Sprintf("%s%s%s", typeMetaDiff, annotationsDiff, dataDiff)
+}
+
+func (configMapsProcessor *ConfigMapsProcessor) updateObject(object client.Object) {
+	object.(*corev1.ConfigMap).TypeMeta.APIVersion = configMapsProcessor.Object.TypeMeta.APIVersion
+	object.(*corev1.ConfigMap).TypeMeta.Kind = configMapsProcessor.Object.TypeMeta.Kind
+	object.(*corev1.ConfigMap).Annotations = configMapsProcessor.Object.Annotations
+	object.(*corev1.ConfigMap).Data = configMapsProcessor.Object.Data
+}
+
+func (configMapsProcessor *ConfigMapsProcessor) IsReady(r processors.Reconcile, log logr.Logger, ctx context.Context, application processors.Application) (bool, error) {
+	return true, nil
+}
+
+func (configMapsProcessor *ConfigMapsProcessor) Process(r processors.Reconcile, log logr.Logger, ctx context.Context, application processors.Application) (bool, error) {
+	return processObject(r, log, ctx, application, configMapsProcessor)
+}
